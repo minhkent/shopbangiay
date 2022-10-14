54d6588fd06090304e1a9970842f24f8ad4203c8
minhkent <minhvu050701@gmail.com>
2022-10-14 10:42:22 +0700

    update search

--- src/components/Searchresponsive.js
+++ src/components/Searchresponsive.js
@@ -1,10 +1,22 @@
-import React ,{useState}from 'react';
+import React ,{useState,useEffect}from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import Search from './Search';
 import { data } from './dataProduct';
 const Searchresponsive = ({setShowSearch}) => {
     const [search,setSearch]  =useState('')
+    const [posts,setPost] = useState([])
+    useEffect(()=>{
+      axios.get('https://633bdef4f11701a65f69bdf0.mockapi.io/api/products')
+      .then(res =>{
+        console.log(res);
+        setPost(res.data)
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+    })
     return (
         <div className='searchresponsive'>
             <div className='overlay'onClick={()=>setShowSearch(true)} ></div>
@@ -16,13 +28,13 @@ const Searchresponsive = ({setShowSearch}) => {
                 </a>
             </div>
             <div className='header-search' >
-                      {data.productData.filter((item)=>{
+                      {posts.filter((item)=>{
                          return search.toLowerCase()===''
                          ? '' : item.title.toLowerCase().includes(search)
                          }).map((item)=>(
                          <Search
                             key ={item.id}
-                            img = {item.img}
+                            img = {item.image}
                             title = {item.title}
                             price = {item.price}
                             item = {item}
